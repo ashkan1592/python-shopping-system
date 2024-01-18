a564bcd29574ab6b587b278a99d730d3b8c3da27
amirali <amiraliqobadi630@gmail.com>
2024-01-18 11:26:14 +0330

    the first template

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python-shopping-system
-python shopping system
+product madule
---
+++ product.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self):
+        pass
+    def add_to_product_list(self):
+        pass
+    def product_list(self):
+        pass
+    def remove_product_list(self):
+        pass
+    def user_validation(self):
+        pass
